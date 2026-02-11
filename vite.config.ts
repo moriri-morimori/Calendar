24726597b72f9ea3da42505ef5e00e9866c2401f
moriri morimori <ayumu17310131@icloud.com>
2026-02-12 01:08:17 +0900

    fix: vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: { watch: { usePolling: true, }, },
   plugins: [react(),tailwind()],
   resolve: {
     alias: {
